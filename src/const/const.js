e20fda09ab1ca00c372fead9788cb1535f04cd3b
MikEight <mickael.4870@gmail.com>
2024-07-18 20:09:24 +0200

    build main components

---
+++ src/const/const.js
@@ -0,0 +1,146 @@
+export const studies = [
+    {
+        title: "Métiers de la relation aux clients et aux usagers",
+        corporation: "LPP le Margériaz - Barberaz (Savoie-France)",
+        date: "09/2012 – 07/2014",
+    },
+    {
+        title: "Développeur Web & Web Mobile",
+        corporation: "O'Clock - Remote",
+        date: "10/2023 – 04/2024",
+    },
+    {
+        title: "Développeur Solana",
+        corporation: "Alyra avec la participation de la Solana Foundation - Remote",
+        date: "06/2024 – 07/2024",
+    },
+];
+
+export const IconKeys = {
+    html: "html",
+    css: "css",
+    javaScript: "javaScript",
+    react: "react",
+    astro: "astro",
+    tailwind: "tailwind",
+    typeScript: "typeScript",
+    git: "git",
+    github: "github",
+    shadcn: "shadcn",
+    nodejs: "nodejs",
+};
+
+export const skills = [
+    {
+        title: "Html",
+        icon: IconKeys.html,
+    },
+    {
+        title: "Css",
+        icon: IconKeys.css,
+    },
+    {
+        title: "JavaScript",
+        icon: IconKeys.javaScript,
+    },
+    {
+        title: "Astro",
+        icon: IconKeys.astro,
+    },
+    {
+        title: "Tailwindcss",
+        icon: IconKeys.tailwind,
+    },
+    {
+        title: "React Js",
+        icon: IconKeys.react,
+    },
+    {
+        title: "Git",
+        icon: IconKeys.git,
+    },
+
+    {
+        title: "Github",
+        icon: IconKeys.github,
+    },
+    {
+        title: "Node Js",
+        icon: IconKeys.nodejs,
+    },
+    {
+        title: "TypeScript",
+        icon: IconKeys.typeScript,
+    },
+    {
+        title: "Shadcn UI",
+        icon: IconKeys.shadcn,
+    },
+];
+
+export const projects = [
+    {
+        img: "../../../img/O’Coloc.png",
+        title: "Cohabit",
+        link: "https://o-coloc-front.onrender.com/",
+        description: `Projet d'école - Développeur back-end & Lead dev back`,
+        technologies: [
+            {
+                icon: IconKeys.typeScript,
+            },
+            {
+                icon: IconKeys.nodejs,
+            },
+            {
+                icon: IconKeys.tailwind,
+            },
+            {
+                icon: IconKeys.javaScript,
+            },
+            {
+                icon: IconKeys.shadcn,
+            },
+        ],
+    },
+    {
+        img: "../../../img/MCS.png",
+        title: "Mon Comptoir Secret",
+        link: "",
+        description: `À venir`,
+        technologies: [
+        ],
+    },
+    {
+        img: "../../../img/ASAP.png",
+        title: "ASAP",
+        link: "",
+        description: `À venir`,
+        technologies: [
+        ],
+    },
+    {
+        img: "../../../img/JL.jpg",
+        title: "Jimmy Lalin - Boxeur Professionnel",
+        link: "",
+        description: `À venir`,
+        technologies: [
+        ],
+    },
+    {
+        img: "../../../img/NovaPay.png",
+        title: "NovaPay",
+        link: "",
+        description: `Projet`,
+        technologies: [
+        ],
+    },
+    {
+        img: "../../../img/Snap Games.png",
+        title: "Snap",
+        link: "",
+        description: `Projet`,
+        technologies: [
+        ],
+    },
+
+];
