9a5acb6bf01e6b3b880669a60f07f1927a18d532
MikEight <mickael.4870@gmail.com>
2024-07-18 15:02:29 +0200

    first commit

---
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { manifest } from './manifest_Cy2pgcDW.mjs';
+import * as serverEntrypointModule from '@astrojs/netlify/ssr-function.js';
+import { onRequest } from './_noop-middleware.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    renderers,
+    middleware: onRequest
+});
+const _args = {
+    "middlewareSecret": "08591ff4-5475-4cc0-b9da-dc228defe086"
+};
+const _exports = serverEntrypointModule.createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
