8486988fe91b58c86211ad5bc8254c5da7dd1577
MikEight <mickael.4870@gmail.com>
2024-07-22 14:54:35 +0200

    attempt to fix bug in deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  }
+  integrations: [],
 });
